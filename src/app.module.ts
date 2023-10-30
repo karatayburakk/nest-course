85d9625e13b61747fd9152668b4c4a4f9b67fb93
karatayburakk <54941630+karatayburakk@users.noreply.github.com>
2023-10-30 16:47:57 +0300

    Global Config and some e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -12,11 +13,26 @@ const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Report],
+        };
+      },
     }),
     UsersModule,
     ReportsModule,
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -15,15 +15,33 @@ describe('Authentication System', () => {
     await app.init();
   });
 
-  it('handles a signup request', async () => {
-    return request(app.getHttpServer())
+  // it('handles a signup request', async () => {
+  //   return request(app.getHttpServer())
+  //     .post('/auth/signup')
+  //     .send({ email: 'asdfa3@asdfa.com', password: 'asdfa' })
+  //     .expect(201)
+  //     .then((res) => {
+  //       const { id, email } = res.body;
+  //       expect(id).toBeDefined();
+  //       expect(email).toEqual('asdfa3@asdfa.com');
+  //     });
+  // });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
       .post('/auth/signup')
-      .send({ email: 'asdfa3@asdfa.com', password: 'asdfa' })
-      .expect(201)
-      .then((res) => {
-        const { id, email } = res.body;
-        expect(id).toBeDefined();
-        expect(email).toEqual('asdfa3@asdfa.com');
-      });
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
